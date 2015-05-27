ef937e738966143471c45fe5143513fb2c169e1b
Steve Manweiler <swm@atlaslearning.net>
2015-05-27 09:34:42 -0500

    Finished library calls: v0.0001

--- librarycalls.go
+++ librarycalls.go
@@ -2,19 +2,33 @@ package GoSDK
 
 import (
 	"fmt"
+	"strconv"
 )
 
 const (
-	_LIB_PREAMBLE = "/codeadmin/v/2/library/"
+	_LIB_PREAMBLE  = "/codeadmin/v/2/library"
+	_HIST_PREAMBLE = "/codeadmin/v/2/history/library"
 )
+
+func (d *DevClient) GetAllLibraries() ([]interface{}, error) {
+	creds, err := d.credentials()
+	if err != nil {
+		return nil, err
+	}
+	resp, err := get(_LIB_PREAMBLE, nil, creds)
+	resp, err = mapResponse(resp, err)
+	if err != nil {
+		return nil, err
+	}
+	return resp.Body.([]interface{}), nil
+}
 
 func (d *DevClient) GetLibraries(systemKey string) ([]interface{}, error) {
 	creds, err := d.credentials()
 	if err != nil {
 		return nil, err
 	}
-	resp, err := get(_LIB_PREAMBLE+systemKey, nil, creds)
-
+	resp, err := get(_LIB_PREAMBLE+"/"+systemKey, nil, creds)
 	resp, err = mapResponse(resp, err)
 	if err != nil {
 		return nil, err
@@ -23,16 +37,80 @@ func (d *DevClient) GetLibraries(systemKey string) ([]interface{}, error) {
 	return resp.Body.([]interface{}), nil
 }
 
+func (d *DevClient) GetLibrary(systemKey, name string) (map[string]interface{}, error) {
+	creds, err := d.credentials()
+	if err != nil {
+		return nil, err
+	}
+	resp, err := get(_LIB_PREAMBLE+"/"+systemKey+"/"+name, nil, creds)
+	resp, err = mapResponse(resp, err)
+	if err != nil {
+		return nil, err
+	}
+	return resp.Body.(map[string]interface{}), nil
+}
+
 func (d *DevClient) CreateLibrary(systemKey, name string, data map[string]interface{}) (map[string]interface{}, error) {
 	creds, err := d.credentials()
 	if err != nil {
 		return nil, err
 	}
-	resp, err := post(_LIB_PREAMBLE+systemKey+"/"+name, data, creds)
+	resp, err := post(_LIB_PREAMBLE+"/"+systemKey+"/"+name, data, creds)
 	resp, err = mapResponse(resp, err)
 	if err != nil {
 		return nil, err
 	}
+	return resp.Body.(map[string]interface{}), nil
+}
 
+func (d *DevClient) UpdateLibrary(systemKey, name string, data map[string]interface{}) (map[string]interface{}, error) {
+	creds, err := d.credentials()
+	if err != nil {
+		return nil, err
+	}
+	resp, err := put(_LIB_PREAMBLE+"/"+systemKey+"/"+name, data, creds)
+	resp, err = mapResponse(resp, err)
+	if err != nil {
+		return nil, err
+	}
+	return resp.Body.(map[string]interface{}), nil
+}
+
+func (d *DevClient) DeleteLibrary(systemKey, name string) error {
+	creds, err := d.credentials()
+	if err != nil {
+		return err
+	}
+	resp, err := delete(_LIB_PREAMBLE+"/"+systemKey+"/"+name, nil, creds)
+	resp, err = mapResponse(resp, err)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (d *DevClient) GetVersionHistory(systemKey, name string) ([]interface{}, error) {
+	creds, err := d.credentials()
+	if err != nil {
+		return nil, err
+	}
+	resp, err := get(_HIST_PREAMBLE+"/"+systemKey+"/"+name, nil, creds)
+	resp, err = mapResponse(resp, err)
+	if err != nil {
+		return nil, err
+	}
+	return resp.Body.([]interface{}), nil
+}
+
+func (d *DevClient) GetVersion(systemKey, name string, version int) (map[string]interface{}, error) {
+	creds, err := d.credentials()
+	if err != nil {
+		return nil, err
+	}
+	resp, err := get(_HIST_PREAMBLE+"/"+systemKey+"/"+name+"/"+strconv.Itoa(version), nil, creds)
+	resp, err = mapResponse(resp, err)
+	if err != nil {
+		return nil, err
+	}
 	return resp.Body.(map[string]interface{}), nil
 }
