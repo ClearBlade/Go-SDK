61e7b61afd0f087ddec2d8a152e3974650332665
Steve Manweiler <swm@atlaslearning.net>
2015-06-18 13:47:18 -0500

    Added calls to access and manipulate trigger timers

--- eventcalls.go
+++ eventcalls.go
@@ -7,6 +7,7 @@ import (
 const (
 	_EVENTS_DEFS_PREAMBLE  = "/api/v/2/triggers/definitions"
 	_EVENTS_HDLRS_PREAMBLE = "/api/v/2/triggers/handlers/"
+	_TIMERS_HDLRS_PREAMBLE = "/api/v/2/triggers/timers/"
 )
 
 func (d *DevClient) GetEventDefinitions() ([]interface{}, error) {
@@ -95,4 +96,71 @@ func mapResponse(resp *CbResp, err error) (*CbResp, error) {
 		return nil, fmt.Errorf(resp.Body.(string))
 	}
 	return resp, nil
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Timer calls are from here down
+
+func (d *DevClient) GetTimers(systemKey string) ([]interface{}, error) {
+	creds, err := d.credentials()
+	if err != nil {
+		return nil, err
+	}
+	resp, err := get(_TIMERS_HDLRS_PREAMBLE+systemKey, nil, creds)
+	resp, err = mapResponse(resp, err)
+	if err != nil {
+		return nil, err
+	}
+	return resp.Body.([]interface{}), nil
+}
+
+func (d *DevClient) GetTimer(systemKey, name string) (map[string]interface{}, error) {
+	creds, err := d.credentials()
+	if err != nil {
+		return nil, err
+	}
+	resp, err := get(_TIMERS_HDLRS_PREAMBLE+systemKey+"/"+name, nil, creds)
+	resp, err = mapResponse(resp, err)
+	if err != nil {
+		return nil, err
+	}
+	return resp.Body.(map[string]interface{}), nil
+}
+
+func (d *DevClient) CreateTimer(systemKey, name string,
+	data map[string]interface{}) (map[string]interface{}, error) {
+	creds, err := d.credentials()
+	if err != nil {
+		return nil, err
+	}
+	resp, err := post(_TIMERS_HDLRS_PREAMBLE+systemKey+"/"+name, data, creds)
+	resp, err = mapResponse(resp, err)
+	if err != nil {
+		return nil, err
+	}
+	return resp.Body.(map[string]interface{}), nil
+}
+
+func (d *DevClient) DeleteTimer(systemKey, name string) error {
+	creds, err := d.credentials()
+	if err != nil {
+		return err
+	}
+	resp, err := delete(_TIMERS_HDLRS_PREAMBLE+systemKey+"/"+name, nil, creds)
+	_, err = mapResponse(resp, err)
+	return err
+}
+
+func (d *DevClient) UpdateTimer(systemKey, name string, data map[string]interface{}) (map[string]interface{}, error) {
+	creds, err := d.credentials()
+	if err != nil {
+		return nil, err
+	}
+	resp, err := put(_TIMERS_HDLRS_PREAMBLE+systemKey+"/"+name, data, creds)
+	resp, err = mapResponse(resp, err)
+	if err != nil {
+		return nil, err
+	}
+	return resp.Body.(map[string]interface{}), nil
 }
