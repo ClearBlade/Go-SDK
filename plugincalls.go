379b86e23477b61d202a9bb5bc54c1f425dc4c10
Michael Sprague <msprague@clearblade.com>
2017-01-11 11:07:34 -0700

    go fmt issues

--- plugincalls.go
+++ plugincalls.go
@@ -1,7 +1,7 @@
 package GoSDK
 
 const (
-	_PLUGINS_USER_PREAMBLE  = "/api/v/2/plugins/"
+	_PLUGINS_USER_PREAMBLE = "/api/v/2/plugins/"
 )
 
 func (d *DevClient) GetPlugins(systemKey string) ([]interface{}, error) {
@@ -56,7 +56,7 @@ func (d *DevClient) UpdatePlugin(systemKey, name string, dash map[string]interfa
 	return resp.Body.(map[string]interface{}), nil
 }
 
-func (d *DevClient) DeletePlugin(systemKey, name string) (map[string]interface{}, error){
+func (d *DevClient) DeletePlugin(systemKey, name string) (map[string]interface{}, error) {
 	creds, err := d.credentials()
 	if err != nil {
 		return nil, err
