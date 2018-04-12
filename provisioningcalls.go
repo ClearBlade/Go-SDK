607eb26cc311a418038f192a52e5868d6dd4aeaf
Steve Manweiler <swm@clearblade.com>
2018-04-12 12:56:31 -0500

    tweaks

--- provisioningcalls.go
+++ provisioningcalls.go
@@ -1,12 +1,16 @@
 package GoSDK
 
-func (d *DevClient) EnterProvisioningMode(systemKey string) (map[string]interface{}, error) {
+import (
+	"fmt"
+)
+
+func (d *DevClient) EnterProvisioningMode() (map[string]interface{}, error) {
 	creds, err := d.credentials()
 	if err != nil {
 		return nil, err
 	}
 
-	resp, err := post(d, "/admin/"+systemKey+"/edgemode/provisioning", map[string]interface{}{}, creds, nil)
+	resp, err := post(d, "/admin/edgemode/provisioning", map[string]interface{}{}, creds, nil)
 	resp, err = mapResponse(resp, err)
 	if err != nil {
 		return nil, err
@@ -15,15 +19,17 @@ func (d *DevClient) EnterProvisioningMode(systemKey string) (map[string]interfac
 	return resp.Body.(map[string]interface{}), nil
 }
 
-func (d *DevClient) EnterRuntimeMode(systemKey string, info map[string]interface{}) (map[string]interface{}, error) {
+func (d *DevClient) EnterRuntimeMode(info map[string]interface{}) (map[string]interface{}, error) {
 	creds, err := d.credentials()
 	if err != nil {
 		return nil, err
 	}
 
-	resp, err := post(d, "/admin/"+systemKey+"/edgemode/runtime", info, creds, nil)
+	resp, err := post(d, "/admin/edgemode/runtime", info, creds, nil)
+	resp, err = mapResponse(resp, err)
 	if err != nil {
 		return nil, err
 	}
+	fmt.Printf("DAMN: %+v\n", resp.Body)
 	return resp.Body.(map[string]interface{}), nil
 }
