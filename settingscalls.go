27ffcfa0dd7305ff9f53b040a8a9bf6891e892cf
Steve Manweiler <swm@clearblade.com>
2020-11-12 15:15:08 -0600

    changed the names of the settings calls

--- settingscalls.go
+++ settingscalls.go
@@ -11,11 +11,11 @@ const (
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-func (d *DevClient) GetGoogleServiceSettings(systemKey string) (map[string]interface{}, error) {
-	return getGoogleServiceSettings(d, _SETTINGS_PREAMBLE+_GOOGLE_SERVICE)
+func (d *DevClient) GetGoogleStorageSettings(systemKey string) (map[string]interface{}, error) {
+	return getGoogleStorageSettings(d, _SETTINGS_PREAMBLE+_GOOGLE_SERVICE)
 }
 
-func getGoogleServiceSettings(c cbClient, endpoint string) (map[string]interface{}, error) {
+func getGoogleStorageSettings(c cbClient, endpoint string) (map[string]interface{}, error) {
 	creds, err := c.credentials()
 	if err != nil {
 		return nil, err
@@ -30,11 +30,11 @@ func getGoogleServiceSettings(c cbClient, endpoint string) (map[string]interface
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-func (d *DevClient) AddGoogleServiceSettings(settings map[string]interface{}) (map[string]interface{}, error) {
-	return addGoogleServiceSettings(d, _SETTINGS_PREAMBLE+_GOOGLE_SERVICE, settings)
+func (d *DevClient) AddGoogleStorageSettings(settings map[string]interface{}) (map[string]interface{}, error) {
+	return addGoogleStorageSettings(d, _SETTINGS_PREAMBLE+_GOOGLE_SERVICE, settings)
 }
 
-func addGoogleServiceSettings(c cbClient, endpoint string, settings map[string]interface{}) (map[string]interface{}, error) {
+func addGoogleStorageSettings(c cbClient, endpoint string, settings map[string]interface{}) (map[string]interface{}, error) {
 	creds, err := c.credentials()
 	if err != nil {
 		return nil, err
@@ -49,11 +49,11 @@ func addGoogleServiceSettings(c cbClient, endpoint string, settings map[string]i
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-func (d *DevClient) UpdateGoogleServiceSettings(settings map[string]interface{}) (map[string]interface{}, error) {
-	return updateGoogleServiceSettings(d, _SETTINGS_PREAMBLE+_GOOGLE_SERVICE, settings)
+func (d *DevClient) UpdateGoogleStorageSettings(settings map[string]interface{}) (map[string]interface{}, error) {
+	return updateGoogleStorageSettings(d, _SETTINGS_PREAMBLE+_GOOGLE_SERVICE, settings)
 }
 
-func updateGoogleServiceSettings(c cbClient, endpoint string, settings map[string]interface{}) (map[string]interface{}, error) {
+func updateGoogleStorageSettings(c cbClient, endpoint string, settings map[string]interface{}) (map[string]interface{}, error) {
 	creds, err := c.credentials()
 	if err != nil {
 		return nil, err
@@ -68,11 +68,11 @@ func updateGoogleServiceSettings(c cbClient, endpoint string, settings map[strin
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-func (d *DevClient) DeleteGoogleServiceSettings(systemKey, deploymentName, box, relPath string) error {
-	return deleteGoogleServiceSettings(d, _SETTINGS_PREAMBLE+_GOOGLE_SERVICE)
+func (d *DevClient) DeleteGoogleStorageSettings(systemKey, deploymentName, box, relPath string) error {
+	return deleteGoogleStorageSettings(d, _SETTINGS_PREAMBLE+_GOOGLE_SERVICE)
 }
 
-func deleteGoogleServiceSettings(c cbClient, endpoint string) error {
+func deleteGoogleStorageSettings(c cbClient, endpoint string) error {
 	creds, err := c.credentials()
 	if err != nil {
 		return err
