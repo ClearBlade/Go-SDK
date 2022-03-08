9aba8b987e0bde7608b9d0bf76c29ec553d057a6
Sanket Deshpande <sanket@clearblade.com>
2022-03-08 10:19:27 -0500

    added user secrets api

---
+++ usersecrets.go
@@ -0,0 +1,57 @@
+package GoSDK
+
+const (
+	_USER_SECRETS_PREAMBLE = "/api/v/4/user_secrets/"
+)
+
+func (d *DevClient) AddSecret(systemKey, name string, data interface{}) (string, error) {
+	creds, err := d.credentials()
+	if err != nil {
+		return "", err
+	}
+	resp, err := post(d, _USER_SECRETS_PREAMBLE+systemKey+"/"+name, data, creds, nil)
+	resp, err = mapResponse(resp, err)
+	if err != nil {
+		return "", err
+	}
+	return resp.Body.(string), nil
+}
+
+func (d *DevClient) UpdateSecret(systemKey, name string, data interface{}) (string, error) {
+	creds, err := d.credentials()
+	if err != nil {
+		return "", err
+	}
+	resp, err := put(d, _USER_SECRETS_PREAMBLE+systemKey+"/"+name, data, creds, nil)
+	resp, err = mapResponse(resp, err)
+	if err != nil {
+		return "", err
+	}
+	return resp.Body.(string), nil
+}
+
+func (d *DevClient) GetSecret(systemKey, name string) (interface{}, error) {
+	creds, err := d.credentials()
+	if err != nil {
+		return "", err
+	}
+	resp, err := get(d, _USER_SECRETS_PREAMBLE+systemKey+"/"+name, nil, creds, nil)
+	resp, err = mapResponse(resp, err)
+	if err != nil {
+		return "", err
+	}
+	return resp.Body, nil
+}
+
+func (d *DevClient) DeleteSecret(systemKey, name string) (string, error) {
+	creds, err := d.credentials()
+	if err != nil {
+		return "", err
+	}
+	resp, err := delete(d, _USER_SECRETS_PREAMBLE+systemKey+"/"+name, nil, creds, nil)
+	resp, err = mapResponse(resp, err)
+	if err != nil {
+		return "", err
+	}
+	return resp.Body.(string), nil
+}
