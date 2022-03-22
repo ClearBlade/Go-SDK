dc6fb0c9364d04e9c2a52796ac4942eefa87cecb
Steve Manweiler <swm@clearblade.com>
2022-03-21 19:42:00 -0500

    added limits call (license)

--- licensecalls.go
+++ licensecalls.go
@@ -22,11 +22,13 @@ func (d *DevClient) ConnectedNodes() ([]string, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	resp, err := get(d, "/admin/connectednodes", nil, creds, nil)
 	resp, err = mapResponse(resp, err)
 	if err != nil {
 		return nil, err
 	}
+
 	return resp.Body.([]string), nil
 }
 
@@ -35,10 +37,26 @@ func (d *DevClient) ConnectedEdges() ([]map[string]interface{}, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	resp, err := get(d, "/admin/connectededges", nil, creds, nil)
 	resp, err = mapResponse(resp, err)
 	if err != nil {
 		return nil, err
 	}
+
 	return resp.Body.([]map[string]interface{}), nil
+}
+
+func (d *DevClient) LicenseLimits() (map[string]interface{}, error) {
+	creds, err := d.credentials()
+	if err != nil {
+		return nil, err
+	}
+
+	limits, err := get(d, "/admin/limits", nil, creds, nil)
+	limits, err = mapResponse(limits, err)
+	if err != nil {
+		return nil, err
+	}
+	return limits.Body.(map[string]interface{}), nil
 }
