e5df8a0a8cb8215a915f5b72e96f29b30cf4f651
Matthew Walowski <mattwalowski@gmail.com>
2024-03-05 09:05:16 -0600

    Add system migration upload endpoint

---
+++ systemmigrationcalls.go
@@ -0,0 +1,31 @@
+package GoSDK
+
+import "fmt"
+
+const (
+	_SYSTEM_MIGRATION_DEV_PREAMBLE = "/api/v/1/systemmigration/"
+)
+
+func (d *DevClient) UploadToSystem(systemKey string, zipBuffer []byte, dryRun bool) (interface{}, error) {
+	creds, err := d.credentials()
+	if err != nil {
+		return nil, err
+	}
+
+	zipContents, err := getContentsForFile(zipBuffer)
+	if err != nil {
+		return nil, err
+	}
+
+	body := map[string]interface{}{
+		"contents": zipContents,
+	}
+
+	url := fmt.Sprintf("%s%s/upload?dryRun=%t", _SYSTEM_MIGRATION_DEV_PREAMBLE, systemKey, dryRun)
+	resp, err := post(d, url, body, creds, nil)
+	resp, err = mapResponse(resp, err)
+	if err != nil {
+		return nil, err
+	}
+	return resp.Body, nil
+}
