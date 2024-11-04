d0e6423ffb37370755488d754a0587ac2dac6a96
Steve Manweiler <swm@clearblade.com>
2024-11-04 12:19:20 -0600

    debug

--- iasidecarcalls.go
+++ iasidecarcalls.go
@@ -1,5 +1,9 @@
 package GoSDK
 
+import (
+	"fmt"
+)
+
 const (
 	_SIDECAR_PREAMBLE_ = "/ia-sidecar"
 )
@@ -9,11 +13,14 @@ func (d *DevClient) CreateIAInstance(name string, data map[string]interface{}) (
 }
 
 func createIAInstance(c cbClient, endpoint string, data map[string]interface{}) (map[string]interface{}, error) {
+	fmt.Printf("createIAInstance\n")
 	creds, err := c.credentials()
+	fmt.Printf("\ncreds: %+v\n\nerr:%+v\n", creds, err)
 	if err != nil {
 		return nil, err
 	}
 	resp, err := post(c, endpoint, data, creds, nil)
+	fmt.Printf("resp: %+v, err: %+v\n", resp, err)
 	resp, err = mapResponse(resp, err)
 	if err != nil {
 		return nil, err
