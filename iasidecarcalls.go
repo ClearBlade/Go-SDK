e9b6aa6f88ef3a37b00a98edf8ceaaa8b9b50b36
Steve Manweiler <swm@clearblade.com>
2024-11-04 08:45:17 -0600

    added ia sidecar stuff

---
+++ iasidecarcalls.go
@@ -0,0 +1,22 @@
+package GoSDK
+
+const (
+	_SIDECAR_PREAMBLE_ = "/ia-sidecar"
+)
+
+func (d *DevClient) CreateIAInstance(systemKey, name string, data map[string]interface{}) (map[string]interface{}, error) {
+	return createIAInstance(d, _SIDECAR_PREAMBLE_+"/instances", data)
+}
+
+func createIAInstance(c cbClient, endpoint string, data map[string]interface{}) (map[string]interface{}, error) {
+	creds, err := c.credentials()
+	if err != nil {
+		return nil, err
+	}
+	resp, err := post(c, endpoint, data, creds, nil)
+	resp, err = mapResponse(resp, err)
+	if err != nil {
+		return nil, err
+	}
+	return resp.Body.(map[string]interface{}), nil
+}
