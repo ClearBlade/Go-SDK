9d632f46879594ae866117ad5821d32762b236ff
Sanket Deshpande <sanket@clearblade.com>
2025-07-09 10:10:13 -0700

    add api to get all mtls system certificates

--- settingscalls.go
+++ settingscalls.go
@@ -158,6 +158,19 @@ func (d *DevClient) GetMTLSSystemCertificate(systemKey, name string) (map[string
 	return resp.Body.(map[string]interface{}), nil
 }
 
+func (d *DevClient) GetAllMTLSSystemCertificates(systemKey string) ([]map[string]interface{}, error) {
+	creds, err := d.credentials()
+	if err != nil {
+		return nil, err
+	}
+	resp, err := get(d, _SETTINGS_PREAMBLE+"mtls/"+systemKey, nil, creds, nil)
+	resp, err = mapResponse(resp, err)
+	if err != nil {
+		return nil, err
+	}
+	return resp.Body.([]map[string]interface{}), nil
+}
+
 func (d *DevClient) DeleteMTLSSystemCertificate(systemKey, name string) error {
 	creds, err := d.credentials()
 	if err != nil {
