91e9548599c770fd0e57b01f7d641463072ae830
Matthew Walowski <mattwalowski@gmail.com>
2025-08-21 13:19:44 -0500

    use query param to pass dst

--- filestorecalls.go
+++ filestorecalls.go
@@ -276,8 +276,8 @@ func copyFilestoreFile(c cbClient, systemKey, filestore, src, dst string) error 
 	if err != nil {
 		return err
 	}
-	endpoint := fmt.Sprintf("%s%s/%s/copy/%s", _FILESTORES_PREAMBLE, systemKey, filestore, src)
-	resp, err := put(c, endpoint, map[string]string{"destination": dst}, creds, nil)
+	endpoint := fmt.Sprintf("%s%s/%s/copy/%s?destination=%s", _FILESTORES_PREAMBLE, systemKey, filestore, src, dst)
+	resp, err := put(c, endpoint, nil, creds, nil)
 	if err != nil {
 		return err
 	}
@@ -304,8 +304,8 @@ func moveFilestoreFile(c cbClient, systemKey, filestore, src, dst string) error 
 	if err != nil {
 		return err
 	}
-	endpoint := fmt.Sprintf("%s%s/%s/move/%s", _FILESTORES_PREAMBLE, systemKey, filestore, src)
-	resp, err := put(c, endpoint, map[string]string{"destination": dst}, creds, nil)
+	endpoint := fmt.Sprintf("%s%s/%s/move/%s?destination=%s", _FILESTORES_PREAMBLE, systemKey, filestore, src, dst)
+	resp, err := put(c, endpoint, nil, creds, nil)
 	if err != nil {
 		return err
 	}
